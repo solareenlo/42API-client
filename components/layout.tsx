504aa3faab21bcaac14d2ef1d23da969a8dc4f25
solareenlo <solareenlo@gmail.com>
2021-03-05 07:53:33 +0900

    fix: title

--- components/layout.tsx
+++ components/layout.tsx
@@ -8,7 +8,7 @@ type Props = {
   title?: string;
 };
 
-const Layout = ({ children, title = 'ft-next' }: Props) => (
+const Layout = ({ children, title = '42api' }: Props) => (
   <div className="max-w-3xl mx-auto px-10 pb-4">
     <Head>
       <title>{title}</title>
@@ -23,7 +23,7 @@ const Layout = ({ children, title = 'ft-next' }: Props) => (
       <meta name="theme-color" content="#ffffff" />
       <meta property="og:url" content="" />
       <meta property="og:type" content="website" />
-      <meta property="og:title" content="ft-next" />
+      <meta property="og:title" content="42api" />
       <meta property="og:description" content="" />
       <meta property="og:image" content='https://og-image.vercel.app/**ft**-next.png?theme=dark&md=1&fontSize=100px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fvercel-triangle-white.svg' />
       <meta name="description" content="" />
